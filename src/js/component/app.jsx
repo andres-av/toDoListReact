443ebdeb5a7675876c63d959ce48cae222442756
andres-av <jandresav1793@gmail.com>
2022-07-28 15:26:39 +0000

    quitar fetch

--- src/js/component/app.jsx
+++ src/js/component/app.jsx
@@ -12,48 +12,18 @@ import AddTask from "./AddTask.jsx";
 // Declare Component
 const App = () => {
 
-    // Server Interaction: 
-    useEffect(() => {
-        const getTasks = async () => {
-            const tasksFromServer = await fetchTasks();
-            setList(tasksFromServer)
-        }
-
-        getTasks()
-    }, []);
-
-    // Fetch tasks
-    const fetchTasks = async () => {
-        const res = await fetch('https://assets.breatheco.de/apis/fake/todos/user/andres-av');
-        const data = await res.json();
-        return data;
-    }
-
     // Declare State variables: 
     const [list, setList] = useState([]);
 
     // Function to add task and ID into list usState array:
-    const addTask = async (task) => {
+    const addTask = (task) => {
         const id = uuidv4();
         const newTask = { id , ...task};
-
-        const res = await fetch('https://assets.breatheco.de/apis/fake/todos/user/andres-av', 
-        {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            }, 
-            body: JSON.stringify(newTask)
-        })
-        const data = res.json();
-
-        setList([...list, data])
+        setList([...list, newTask])
     };
 
     // Function to delete each task, called by the onClick event on each <li>
-    const handleDeleteTask = async (id) => {
-        await fetch(`https://assets.breatheco.de/apis/fake/todos/user/andres-av/${id}`, 
-        { method: 'DELETE'})
+    const handleDeleteTask = (id) => {
         setList(list.filter((task) => task.id !== id));
     }
 
